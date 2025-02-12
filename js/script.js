71dcbe05146c4945590f526bdfe15c97f181e800
Ali Nassar <aliinassar8@gmail.com>
2025-02-13 00:46:41 +0200

    Activating the scroll on click and activating the clicked dot for projects map

--- js/script.js
+++ js/script.js
@@ -98,3 +98,27 @@ window.onscroll = () => {
     isDataUpdated = true;
   }
 };
+
+// projects 
+
+const buttonsWrapper = document.querySelector(".map");
+const slides = document.querySelector(".projects-box");
+
+buttonsWrapper.addEventListener("click", e => {
+  if (e.target.nodeName === "BUTTON") {
+    Array.from(buttonsWrapper.children).forEach(item =>
+      item.classList.remove("active")
+    );
+    if (e.target.classList.contains("first")) {
+      slides.style.transform = "translateX(-0%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("second")) {
+      slides.style.transform = "translateX(-33%)";
+      e.target.classList.add("active");
+    }
+    //  else if (e.target.classList.contains('third')){
+    //   slides.style.transform = 'translatex(-66%)';
+    //   e.target.classList.add('active');
+    // }
+  }
+});
